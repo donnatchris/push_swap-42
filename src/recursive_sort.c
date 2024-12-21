0c593cb4814080e3fe602fe3b16446c4039bf48c
chdonnat <donnatchris@live.fr>
2024-12-21 09:37:53 +0100

    still finding a way to optimize recursive_sort - need to check if portion of stack is already sorted in each sort function

--- src/recursive_sort.c
+++ src/recursive_sort.c
@@ -6,7 +6,7 @@
 /*   By: christophedonnat <christophedonnat@stud    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/20 08:44:22 by christophed       #+#    #+#             */
-/*   Updated: 2024/12/21 08:31:14 by christophed      ###   ########.fr       */
+/*   Updated: 2024/12/21 08:43:21 by christophed      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 // Function to find the median value in the stack
 int	pivot_value(t_stack *head, int len)
 {
-	// printf("PIVOT VALUE\n");
+	// printf("BEFORE PIVOT VALUE\n");
 	t_stack	*current;
 	t_stack *pivot;
 	int		n_inferior_values;
@@ -47,7 +47,9 @@ int	pivot_value(t_stack *head, int len)
 // Function to push all values below treshold to b
 int	divide_first_a_by_treshold(t_stack **a, t_stack **b, int len, int treshold)
 {
-	// printf("DIVIDE FIRST A BY TRESHOLD\n");
+	printf("before DIVIDE FIRST A BY TRESHOLD\n");
+	dclst_print(*a);
+	dclst_print(*b);
 	int		pushed;
 
 	pushed = 0;
@@ -68,7 +70,9 @@ int	divide_first_a_by_treshold(t_stack **a, t_stack **b, int len, int treshold)
 // Function to push all values below treshold to b
 int	divide_a_by_treshold(t_stack **a, t_stack **b, int len, int treshold)
 {
-	// printf("DIVIDE A BY TRESHOLD\n");
+	printf("before DIVIDE A BY TRESHOLD\n");
+	dclst_print(*a);
+	dclst_print(*b);
 	int		pushed;
 	int		set_aside;
 
@@ -99,7 +103,9 @@ int	divide_a_by_treshold(t_stack **a, t_stack **b, int len, int treshold)
 // Function to push all values below treshold to a
 int	divide_b_by_treshold(t_stack **a, t_stack **b, int len, int treshold)
 {
-	// printf("DIVIDE B BY TRESHOLD\n");
+	printf("before DIVIDE B BY TRESHOLD\n");
+	dclst_print(*a);
+	dclst_print(*b);
 	int		pushed;
 	int		set_aside;
 
@@ -130,7 +136,9 @@ int	divide_b_by_treshold(t_stack **a, t_stack **b, int len, int treshold)
 // Function to get back the values from stack_a to stack_b
 int	get_back_to_b(t_stack **a, t_stack **b, int len)
 {
-	// printf("GET BACK TO B\n");
+	printf("before GET BACK TO B\n");
+	dclst_print(*a);
+	dclst_print(*b);
 	while (len > 0)
 	{
 		if (PB < 0)
@@ -142,7 +150,9 @@ int	get_back_to_b(t_stack **a, t_stack **b, int len)
 // Function to get back the values from stack_b to stack_a
 int	get_back_to_a(t_stack **a, t_stack **b, int len)
 {
-	// printf("GET BACK TO A\n");
+	printf("before GET BACK TO A\n");
+	dclst_print(*a);
+	dclst_print(*b);
 	while (len > 0)
 	{
 		if (PA < 0)
@@ -155,7 +165,9 @@ int	get_back_to_a(t_stack **a, t_stack **b, int len)
 // Function to sort the remainder in stack_b
 int	sort_b(t_stack **a, t_stack **b, int len)
 {
-	// printf("SORT B\n");
+	printf("before SORT B\n");
+	dclst_print(*a);
+	dclst_print(*b);
 	int		treshold;
 	int		pushed;
 
@@ -185,7 +197,9 @@ int	sort_b(t_stack **a, t_stack **b, int len)
 // Function to sort the remainder in stack_a
 int	sort_a(t_stack **a, t_stack **b, int len)
 {
-	// printf("SORT A\n");
+	printf("before SORT A\n");
+	dclst_print(*a);
+	dclst_print(*b);
 	int		treshold;
 	int		pushed;
 
@@ -213,7 +227,9 @@ int	sort_a(t_stack **a, t_stack **b, int len)
 // Function to sort stack_a
 int	first_sort_a(t_stack **a, t_stack **b, int len)
 {
-	// printf("FIRST SORT A\n");
+	printf("FIRST SORT A\n");
+	dclst_print(*a);
+	dclst_print(*b);
 	int		treshold;
 	int		pushed;
 
@@ -248,7 +264,7 @@ int	push_swap(t_stack **a, t_stack **b)
 		return (sort_3_or_less(a), dclst_clear(a), 0);
 	if (first_sort_a(a, b, len) < 0)
 		return (dclst_clear(a), dclst_clear(b), -1);
-	if (!is_sorted(*a))
+	if (!is_sorted(*a) || *b)
 		write(1, "Sort has failed\n", 16);
 	printf("SORT REUSSI\n");
 	printf("STACK A\n");
